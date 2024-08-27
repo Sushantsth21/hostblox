72f82e9553b4ec77e81283b39db39b895c9cb35c
Sushant Shrestha <sushant.shrestha003@gmail.com>
2024-08-26 20:48:55 -0400

    firbase config and links

--- app/src/app/host/page.js
+++ app/src/app/host/page.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Create from "./create";
+import Join from "./join";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Create />
+      <Join />
+    </div>
+  );
 };
 
 export default page;
